619081d1b1e34d5354d44fe7275cf7b2aabbd7b7
Levi Starrett <levi@roxsoftware.com>
2016-02-13 12:27:00 -0500

    job #8073 updated command line interface, added project parsing

--- src/masl_parser/src/MaslImportParser.java
+++ src/masl_parser/src/MaslImportParser.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
+import java.util.regex.Pattern;
 
 public class MaslImportParser {
 
@@ -28,13 +29,6 @@ public class MaslImportParser {
     // public getter for current_file
     public String getFile() {
         return current_file;
-    }
-
-    public void parseAll( String[] files ) {
-        // parse all MASL files
-        for ( String file : files ) {
-            parse( "target", file );
-        }
     }
 
     // parse a MASL file
@@ -141,6 +135,196 @@ public class MaslImportParser {
 
     }
 
+    // parse a single MASL file
+    public void parseFile( String rule, String fn, String out ) {
+
+        PrintStream     output = System.out;
+
+        // check args
+        if ( null == rule || null == fn ) {
+            System.err.println( "-parseFile: ERROR invalid arguments" );
+            return;
+        }
+
+        // set output
+        if ( null == out ) {
+            setOutput( System.out );
+        }
+        else if ( !out.equals( "auto" ) ) {
+            try {
+                output = new PrintStream( out );
+            } catch ( FileNotFoundException e ) {
+                System.err.println( "-parse: ERROR " + e );
+            }
+            setOutput( output );
+        }
+        else {
+            try {
+                output = new PrintStream( fn + ".masldsl" );
+            } catch ( FileNotFoundException e ) {
+                System.err.println( "-parse: ERROR " + e );
+            }
+            setOutput( output );
+        }
+
+        // parse the file
+        parse( rule, fn );
+
+        // done
+    }
+    // parse a MASL domain
+    public void parseDomain( String directory, String out ) {
+        File            dir;
+        File[]          domainFiles;
+        
+        PrintStream     output = System.out;
+
+        String          domainName;
+
+        // check args
+        if ( null == directory ) {
+            System.err.println( "-parse: ERROR null directory" );
+            return;
+        }
+
+        // set output
+        if ( null == out ) {
+            setOutput( System.out );
+        }
+        else if ( !out.equals( "auto" ) ) {
+            try {
+                output = new PrintStream( out );
+            } catch ( FileNotFoundException e ) {
+                System.err.println( "-parse: ERROR " + e );
+            }
+            setOutput( output );
+        }
+
+        // read the directory
+        dir = new File( directory );
+        domainFiles = dir.listFiles();
+        if ( null == directory ) {
+            System.err.println( "-listFiles: ERROR" );
+            return;
+        }
+
+        // parse the domain file
+        for ( File f : domainFiles ) {
+            if ( Pattern.matches( ".*\\.mod", f.getName() ) ) {
+
+                if ( null != out && out.equals( "auto" ) ) {
+                    // set the domain name
+                    domainName = f.getName().substring( 0, f.getName().length() - 4 );
+                    try {
+                        output = new PrintStream( domainName + ".masldsl" );
+                    } catch ( FileNotFoundException e ) {
+                        System.err.println( "-parse: ERROR " + e );
+                    }
+                    setOutput( output );
+                }
+
+                // parse the file
+                parse( "target", f.getPath() );
+                break;
+            }
+        }
+
+        // parse all activities ( according the defined file extension convention )
+        for ( File f : domainFiles ) {
+            if ( Pattern.matches( ".*\\.svc", f.getName() ) ||
+                 Pattern.matches( ".*\\.fn", f.getName() ) ||
+                 Pattern.matches( ".*\\.ext", f.getName() ) ||
+                 Pattern.matches( ".*\\.scn", f.getName() ) ||
+                 Pattern.matches( ".*\\.al", f.getName() ) ||
+                 Pattern.matches( ".*\\.tr", f.getName() ) ) {
+                     
+                // parse the file
+                parse( "activityDefinition", f.getPath() );
+            }
+        }
+
+        // done
+    }
+
+    // parse a MASL project
+    public void parseProject( String directory, String out ) {
+        File            dir;
+        File[]          projectFiles;
+        
+        PrintStream     output = System.out;
+
+        String          projectName;
+
+        // check args
+        if ( null == directory ) {
+            System.err.println( "-parse: ERROR null directory" );
+            return;
+        }
+
+        // set output
+        if ( null == out ) {
+            setOutput( System.out );
+        }
+        else if ( !out.equals( "auto" ) ) {
+            try {
+                output = new PrintStream( out );
+            } catch ( FileNotFoundException e ) {
+                System.err.println( "-parse: ERROR " + e );
+            }
+            setOutput( output );
+        }
+
+        // read the directory
+        dir = new File( directory );
+        projectFiles = dir.listFiles();
+        if ( null == directory ) {
+            System.err.println( "-listFiles: ERROR" );
+            return;
+        }
+
+        // parse the project file
+        for ( File f : projectFiles ) {
+            if ( Pattern.matches( ".*\\.prj", f.getName() ) ) {
+
+                if ( null != out && out.equals( "auto" ) ) {
+                    // set the project name
+                    projectName = f.getName().substring( 0, f.getName().length() - 4 );
+                    try {
+                        output = new PrintStream( projectName + ".masldsl" );
+                    } catch ( FileNotFoundException e ) {
+                        System.err.println( "-parse: ERROR " + e );
+                    }
+                    setOutput( output );
+                }
+
+                // parse the file
+                parse( "target", f.getPath() );
+                break;
+            }
+        }
+
+        // parse all activities ( according the defined file extension convention )
+        for ( File f : projectFiles ) {
+            if ( Pattern.matches( ".*\\.tr", f.getName() ) ) {
+                // parse the file
+                parse( "activityDefinition", f.getPath() );
+            }
+        }
+
+        // done
+    }
+
+    // print usage
+    public void printUsage() {
+        System.err.println("Usage:\n");
+        System.err.println("Parse single MASL file:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser -f <rule> <MASL file> [-o [file name] ]\n");
+        System.err.println("Parse MASL domain:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser -d <domain directory> [-o [file name] ]\n");
+        System.err.println("Parse MASL project:");
+        System.err.println("\tjava -cp <classpath> MaslImportParser -p <project directory> [-o [file name] ]");
+    }
+
     // main method
     public static void main(String args[]) throws Exception {
 
@@ -149,16 +333,76 @@ public class MaslImportParser {
 
         // check input args
         if ( args.length < 1 ) {
-            System.out.println( "No MASL file provided." );
+            // print usage
+            parser.printUsage();
+        }
+        else {
+            if ( args[0].equals( "-f" ) ) {             // parse single MASL file
+                String out = null;
+                if ( args.length == 3 ) {
+                    out = null;
+                }
+                else if ( args.length == 4 && args[3].equals( "-o" ) ) {
+                    out = "auto";
+                }
+                else if ( args.length == 5 && args[3].equals( "-o" ) ) {
+                    out = args[4];
+                }
+                else {
+                    // print usage
+                    parser.printUsage();
+                    return;
+                }
+
+                // parse the file
+                parser.parseFile( args[1], args[2], out );
+            }
+            else if ( args[0].equals( "-d" ) ) {        // parse MASL domain
+                String out = null;
+                if ( args.length == 2 ) {
+                    out = null;
+                }
+                else if ( args.length == 3 && args[2].equals( "-o" ) ) {
+                    out = "auto";
+                }
+                else if ( args.length == 4 && args[2].equals( "-o" ) ) {
+                    out = args[3];
+                }
+                else {
+                    // print usage
+                    parser.printUsage();
+                    return;
+                }
+
+                // parse the domain
+                parser.parseDomain( args[1], out );
+            }
+            else if ( args[0].equals( "-p" ) ) {        // parse MASL project
+                String out = null;
+                if ( args.length == 2 ) {
+                    out = null;
+                }
+                else if ( args.length == 3 && args[2].equals( "-o" ) ) {
+                    out = "auto";
+                }
+                else if ( args.length == 4 && args[2].equals( "-o" ) ) {
+                    out = args[3];
+                }
+                else {
+                    // print usage
+                    parser.printUsage();
+                    return;
+                }
+
+                // parse the domain
+                parser.parseProject( args[1], out );
+            }
+            else {
+                // print usage
+                parser.printUsage();
+            }
         }
 
-        // parse all MASL files
-        //parser.parseAll( args );
-
-        if ( args.length < 2 )
-            parser.parse( "target", args[0] );
-        else
-            parser.parse( args[0], args[1] );
     }
 }
 
